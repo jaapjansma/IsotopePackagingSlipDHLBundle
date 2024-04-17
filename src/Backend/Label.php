2fd6aebddf9787c2c8f5c395ae68737565be09f6
Jaap Jansma <jaap.jansma@civicoop.org>
2024-04-17 17:00:12 +0200

    update na testen

--- src/Backend/Label.php
+++ src/Backend/Label.php
@@ -138,7 +138,7 @@ class Label extends Backend {
   protected function prepareFileName($strName)
   {
     // Replace simple tokens
-    $strName = $this->sanitizeFileName(StringUtil::recursiveReplaceTokensAndTags($strName, StringUtil::NO_TAGS | StringUtil::NO_BREAKS | StringUtil::NO_ENTITIES));
+    $strName = $this->sanitizeFileName(StringUtil::recursiveReplaceTokensAndTags($strName, [], StringUtil::NO_TAGS | StringUtil::NO_BREAKS | StringUtil::NO_ENTITIES));
     return $strName;
   }
 
